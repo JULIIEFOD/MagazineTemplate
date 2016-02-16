aa0c534d3887b05d75d882ac636ef95dc0ba84fb
JuliaFrich <uliafrich@gmail.com>
2016-02-16 02:52:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+document.querySelector('.fb-like-btn').addEventListener('click', function(){
+			this.parentNode.querySelector('.fb-counter').innerText++;
+
+		});
+
+
+
+document.querySelector('.g-like-btn').addEventListener('click', function(){
+			this.parentNode.querySelector('.google-counter').innerText++;
+
+		});
